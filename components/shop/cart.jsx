6026d4304e7a3c103793b0c0b8526b84542b2ca2
microdotmatrix <liquid.knives.2@gmail.com>
2022-11-17 09:07:29 -0600

    non

--- components/shop/cart.jsx
+++ components/shop/cart.jsx
@@ -4,20 +4,25 @@ import { useEffect } from 'react'
 import { useCart } from 'react-use-cart'
 import Image from 'next/image'
 
-export default function Cart() {
+export default function Cart(props) {
   const {
     isEmpty,
     totalUniqueItems,
     items,
+    totalItems,
+    cartTotal,
     updateItemQuantity,
-    removeItem
+    removeItem,
+    emptyCart
   } = useCart();
+
+  const [itemSubTotal, setItemSubTotal] = useState(1);
 
   if (isEmpty) return <p>Your cart is empty</p>;
 
   return (
     <>
-      <h2>Cart ({totalUniqueItems})</h2>
+      <h2>Cart ({totalUniqueItems}) - Total {totalItems}</h2>
 
       <ul>
         {items?.map((item) => (
@@ -32,6 +37,7 @@ export default function Cart() {
                 />
               </div>
               <h4>{item.name}</h4>
+              <span>{item.price}</span>
               <div className='flex-1 flex flex-row justify-end'>
                 <div>
                   <h5 className='text-slate-500'>Qty:</h5>
@@ -47,12 +53,23 @@ export default function Cart() {
                     +
                   </button>
                 </div>
+                <div>
+                  <span className="item-subtotal"></span>
+                </div>
                 <button onClick={() => removeItem(item.id)}>&times;</button>
                 </div>
             </div>
           </li>
         ))}
       </ul>
+      <h3>Total Price: {cartTotal}</h3>
+      <button onClick={() => emptyCart()}>Empty Cart</button>
+
     </>
   );
 }
+
+
+
+
+
