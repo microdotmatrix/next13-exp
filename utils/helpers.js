6362b300fc601406444d184d4b1f2ac6c7552c94
microdotmatrix <liquid.knives.2@gmail.com>
2022-11-25 13:56:31 -0600

    fd

---
+++ utils/helpers.js
@@ -0,0 +1,58 @@
+"use client"
+
+import { useState } from 'react'
+
+export function useHasMounted() {
+  const [hasMounted, setHasMounted] = useState(false)
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  return hasMounted
+}
+
+/*  ------------------------------ */
+/*  Image helpers
+/*  ------------------------------ */
+
+export function buildSrc(image, { width, height, format, quality }) {
+  let imgSrc = imageBuilder.image(image)
+
+  if (width) {
+    imgSrc = imgSrc.width(Math.round(width))
+  }
+
+  if (height) {
+    imgSrc = imgSrc.height(Math.round(height))
+  }
+
+  if (format) {
+    imgSrc = imgSrc.format(format)
+  }
+
+  if (quality) {
+    imgSrc = imgSrc.quality(quality)
+  }
+
+  return imgSrc.fit('max').auto('format').url()
+}
+
+export function buildSrcSet(image, { srcSizes, aspect, format, quality }) {
+  const sizes = srcSizes.map((width) => {
+    let imgSrc = buildSrc(image, {
+      ...{ width },
+      height: aspect && Math.round(width * aspect) / 100,
+      ...{ format },
+      ...{ quality },
+    })
+
+    if (format) {
+      imgSrc = imgSrc.format(format)
+    }
+
+    return `${imgSrc} ${width}w`
+  })
+
+  return sizes.join(',')
+}
